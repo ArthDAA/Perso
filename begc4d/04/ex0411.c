f2f62a8ea07db79cfc69d4fef1f4868e99454e1f
ArthDAA <arthus.daa06@gmail.com>
2024-10-16 17:07:04 +0200

    Legere progression

---
+++ begc4d/04/ex0411.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Etranger dans son propre pays");
+    return (0);
+}
---
+++ begc4d/04/ex0413.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Une souris verte,\n");
+    puts("qui courait dans l'herbe.\n");
+    puts("Je l'attrape par la queue,\n");
+    puts("Je la montre a ces messieurs.\n");
+    puts("Ces messieurs me disent");
+    return(0);
+}
