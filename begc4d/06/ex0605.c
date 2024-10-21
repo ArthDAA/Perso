f7220a264394f56bc92d905ae5ceab30cf812504
ArthDAA <arthus.daa06@gmail.com>
2024-10-21 16:43:11 +0200

    Actualisation - Chapitre 06 en cours

---
+++ begc4d/06/ex0605.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int blorf = 22;
+
+int main()
+{
+    printf("La valeur de blorf est %d.\n", blorf);
+    printf("La valeur de blorf + 16 est %d.\n", blorf + 16);
+    printf("La valeur de blorf puissance deux est %d.\n", blorf * blorf);
+    return (0);
+}
---
+++ begc4d/06/ex0606.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int blorf = 22;
+
+int GLORKUS = 16;
+
+int main()
+{
+    printf("La valeur de blorf est %d.\n", blorf);
+    printf("La valeur de blorf + 16 est %d.\n", blorf + GLORKUS);
+    printf("La valeur de blorf puissance deux est %d.\n", blorf * blorf);
+    return (0);
+}
