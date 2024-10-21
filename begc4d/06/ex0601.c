6066571cf1ed8e1d5d41b9ad45aa117ce5a8a0d5
ArthDAA <arthus.daa06@gmail.com>
2024-10-21 09:52:50 +0200

    Actualisation ch 06

---
+++ begc4d/06/ex0601.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;                                                  //L05
+
+    x = 5;                                                  //L07
+    printf("La valeur de la variable x est %d\n", x);       //L08
+    return (0);
+}
