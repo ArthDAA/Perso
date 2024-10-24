83fc5c1b5199fedf813ab15abff6ff97e17d55a5
ArthDAA <arthus.daa06@gmail.com>
2024-10-24 15:58:50 +0200

    ajout ex03 disparu - Actualisation

--- begc4d/07/ex0703.c
+++ begc4d/07/ex0703.c
@@ -1,1 +1,11 @@
-m
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    printf("Je vais recevoir un caractere: ");
+    c = getc(stdin);
+    printf("J'ai obtenu le caractere'%c'\n", c);
+    return (0);
+}
