db2838106f5f2aecd7e51c3f678b88cd91a9ca60
ArthDAA <arthus.daa06@gmail.com>
2024-11-08 05:45:35 +0100

    added ex0714 15

---
+++ begc4d/07/ex0714.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int fav;
+    printf("Saisissez votre chiffre favori : ");
+    scanf("%d", &fav);
+    printf("%d est mon chiffre favori aussi !\n", fav);     //L09
+    return (0);
+}
