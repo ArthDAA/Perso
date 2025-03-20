596f97183ebe83e6b28db5d8f2c7a2263826971c
ArthDAA <arthus.daa06@gmail.com>
2025-03-20 15:09:04 +0100

    C02 valide et a la norme

--- 42/C02/ex09/ft_strcapitalize.c
+++ 42/C02/ex09/ft_strcapitalize.c
@@ -6,33 +6,49 @@
 /*   By: arthdaa06 <arthus.daa06@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 05:54:41 by arthdaa06         #+#    #+#             */
-/*   Updated: 2025/03/19 11:17:45 by arthdaa06        ###   ########.fr       */
+/*   Updated: 2025/03/20 15:08:12 by arthdaa06        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_uppercase(char str)
+int	ft_str_is_alphanum(char c)
 {
-	if (str < 'A' || str > 'Z')
-		return (0);
-	return (1);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
 }
 
-int	ft_str_is_lowecase(char str)
+void	ft_strlowcase(char *c)
 {
-	if (str < 'a' || str > 'z')
-		return (0);
-	return (1);
+	if (*c >= 'A' && *c <= 'Z')
+		*c += 32;
 }
 
-char	*ft_strlowcase(char *str)
+void	ft_strupcase(char *c)
+{
+	if (*c >= 'a' && *c <= 'z')
+		*c -= 32;
+}
+
+char	*ft_strcapitalize(char *str)
 {
 	int	i;
+	int	new_word;
 
 	i = 0;
+	new_word = 1;
 	while (str[i] != '\0')
 	{
-		if (ft_str_is_uppercase(str[i]) == 1)
-			str[i] += 32;
+		if (ft_str_is_alphanum(str[i]))
+		{
+			if (new_word)
+				ft_strupcase(&str[i]);
+			else
+				ft_strlowcase(&str[i]);
+			new_word = 0;
+		}
+		else
+			new_word = 1;
 		i++;
 	}
 	return (str);
