fb7bec2997035898cd2c7ec22f7b012b0453bcc8
ArthDAA <arthus.daa06@gmail.com>
2025-05-01 04:05:45 +0200

    tkt

--- 42/C02/ex10/ft_strlcpy.c
+++ 42/C02/ex10/ft_strlcpy.c
@@ -1,25 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arthdaa06 <arthus.daa06@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/15 18:11:10 by arthdaa06         #+#    #+#             */
-/*   Updated: 2025/03/21 06:59:36 by arthdaa06        ###   ########.fr       */
+/*   Created: 2025/03/21 04:47:01 by arthdaa06         #+#    #+#             */
+/*   Updated: 2025/03/21 06:19:33 by arthdaa06        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putstr(char *str)
+/*I'm SO SORRY for this terrible indentation. Fkng Norminette bug.*/
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	int	i;
+	unsigned int	j;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
+	j = 0;
+	while (src[i])
 		i++;
+	if (size > 0)
+	{
+		while (j < size - 1 && src[j])
+		{
+			dest[j] = src[j];
+			j++;
+		}
+		dest[j] = '\0';
+		}
+			return (i);
 	}
-}
