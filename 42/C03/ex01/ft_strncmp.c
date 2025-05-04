6d20efdbbb4310dfefa86ee2648377517d8d0143
ArthDAA <arthus.daa06@gmail.com>
2025-05-04 17:42:01 +0200

    norme

--- 42/C03/ex01/ft_strncmp.c
+++ 42/C03/ex01/ft_strncmp.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arthdaa06 <arthus.daa06@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/21 04:47:01 by arthdaa06         #+#    #+#             */
-/*   Updated: 2025/05/04 17:39:40 by arthdaa06        ###   ########.fr       */
+/*   Created: 2025/05/04 17:16:25 by arthdaa06         #+#    #+#             */
+/*   Updated: 2025/05/04 17:30:30 by arthdaa06        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*I'm SO SORRY for this terrible indentation. Fkng Norminette bug.*/
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int		i;
-	unsigned int	j;
+	unsigned int	i;
 
 	i = 0;
-	j = 0;
-	while (src[i])
+	while (i < n && s1[i] == s2[i] && s1[i] != '\0')
 		i++;
-	if (size > 0)
-	{
-		while (j < size - 1 && src[j])
-		{
-			dest[j] = src[j];
-			j++;
-		}
-		dest[j] = '\0';
-	}
-	return (i);
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
